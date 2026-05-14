18c3c269f11d4b1f347cf9fa5dff517655becfcb
sanjit <sanjit.est14@gmail.com>
2026-05-14 11:27:37 +0600

    CountZero by multiple pointer

---
+++ 5_problem_solving_pattern/sumZero.js
@@ -0,0 +1,13 @@
+// Time Complexity - O(N^2)
+
+// Space Complexity - O(1)
+
+function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
