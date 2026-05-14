66adf1dfd55db67eeb29fb353c8033a86d2572c9
sanjit <sanjit.est14@gmail.com>
2026-05-14 14:21:23 +0600

    Divede and conquire pattern

---
+++ 5_problem_solving_pattern/serch_nieve.js
@@ -0,0 +1,12 @@
+//Linear Search
+
+// Time Complexity O(N)
+
+function search(arr, val){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === val){
+            return i;
+        }
+    }
+    return -1;
+}
